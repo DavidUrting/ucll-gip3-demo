df05d2b60eb4ee8353f90fa157fca1e58ed7011e
David Urting <david.urting@live.com>
2020-05-02 01:20:17 +0200

    CustomerViewModel

---
+++ GiP3Demo/Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GiP3Demo.Models;
+using GiP3Demo.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GiP3Demo.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private AdventureWorks2017Context _context;
+
+        public CustomerController(AdventureWorks2017Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<CustomerViewModel> model = from c in _context.Customer
+                                                   .Include(c => c.Person)
+                                                   .ThenInclude(p => p.BusinessEntity)
+                                                   .ThenInclude(be => be.BusinessEntityAddress)
+                                                   .ThenInclude(bea => bea.Address)
+                                                   select new CustomerViewModel()
+                                                   {
+                                                       FirstName = c.Person.FirstName,
+                                                       LastName = c.Person.LastName,
+                                                       AccountNumber = c.AccountNumber,
+                                                       City = c.Person.BusinessEntity.BusinessEntityAddress.FirstOrDefault() != null ? c.Person.BusinessEntity.BusinessEntityAddress.FirstOrDefault().Address.City : "n.b."
+                                                   };
+
+            return View(model);
+        }
+    }
+}
---
+++ GiP3Demo/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiP3Demo.ViewModels
+{
+    public class CustomerViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AccountNumber { get; set; }
+        public string City { get; set; }
+    }
+}
